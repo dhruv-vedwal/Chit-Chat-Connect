6a58b91fdf3c65703e6e192933888df097408052
Dhruv Vedwal <vedwaldhruv12@gmail.com>
2024-03-18 15:07:41 +0530

    Final Project Commit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes")
@@ -26,7 +25,7 @@ app.use('/api/message', messageRoutes)
 //---------------------Deployment---------------------
 
 const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === "production") {
+if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname1, "/frontend/build")));
     app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
@@ -42,7 +41,7 @@ if(process.env.NODE_ENV === "production") {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 
